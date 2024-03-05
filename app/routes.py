711dd4ef2c620d4db26e16c906776320555d61d1
LanternNassi <Lanternnassi@gmail.com>
2024-03-05 10:23:21 +0300

    Adding authorization to the service

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,13 @@
-from fastapi import APIRouter, Body
+from fastapi import APIRouter, Body , Header, HTTPException
 
 import app.controllers as controllers
 from app.model import Activity
 from fastapi import Query
-from typing import Optional
+from typing import Optional , Annotated
 from app.tasks import add_activity_task, hello
+
+from app.helpers.decorators import admin_required , authenticate
+
 
 
 router = APIRouter()
@@ -17,13 +20,16 @@ async def index():
 
 
 @router.post("/activities")
-def add_activity(data: Activity = Body(...)):
+@authenticate
+def add_activity(access_token: Annotated[str | None, Header()] = None , data: Activity = Body(...)):
     add_activity_task.delay(data.dict())
     return {"message": "activity added successfully"}
 
 
 @router.get("/activities")
+@authenticate
 def get_activities(
+        access_token: Annotated[str | None, Header()] = None,
         operation: Optional[str] = Query(
             None, description="Operation"),
         status: Optional[str] = Query(None, description="Status"),
@@ -52,6 +58,7 @@ def get_activities(
         a_app_id=a_app_id,
         start=start,
         end=end)
+
 
 
 @router.get("/activities/{activity_id}", response_model=Activity)
