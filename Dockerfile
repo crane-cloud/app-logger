975ac7bfa2b15ad46bb54e2ae633c0f108432fd5
Mubangizi <mubangizia22@gmail.com>
2024-05-15 10:55:27 +0300

    start app with scrip file

--- Dockerfile
+++ Dockerfile
@@ -24,5 +24,5 @@ COPY . /celery-app
 
 EXPOSE 8000
 
-CMD ["poetry", "run", "uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000", "--reload"]
+ENTRYPOINT ["sh", "/app/scripts/start.sh"]
 
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env bash
 #!/bin/bash
 
-
 # Start the server
-poetry run python uvicorn main:app --host 0.0.0.0 --port 8000 --workers 4
+poetry run uvicorn main:app --host 0.0.0.0 --port 8000 --reload
