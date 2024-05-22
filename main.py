d9bac27c07bd690ed58d9d1f2839773da6fdd961
khalifan-kfan <khalifanmuwonge@gmail.com>
2024-05-22 22:23:00 +0300

    ch:disable cors error

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from fastapi import FastAPI
 from app.tasks import create_celery
 from app import routes
+from fastapi.middleware.cors import CORSMiddleware
 
 
 def create_app() -> FastAPI:
     app = FastAPI()
 
     app.celery_app = create_celery()
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     app.include_router(routes.router, prefix="/api")
 
@@ -15,4 +24,3 @@ def create_app() -> FastAPI:
 
 app = create_app()
 celery = app.celery_app
-
